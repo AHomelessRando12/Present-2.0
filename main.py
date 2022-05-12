c2a1a61bea142082bfb816b7846edee4b5e4c7f0
AHomelessRando12 <85993071+AHomelessRando12@users.noreply.github.com>
2022-05-12 12:10:52 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ randomNum = random.randint(1, 5)
 guess = int(input("You get one try to guess a number between one and five. If you guess wrong, this computer will "
                   "delete everything... Guess: "))
 if guess != randomNum:
-    os.system("logoff -s -t 10")
-    print("You guessed wrong! Luckily I will only log you off this time...")
+    os.system("shutdown -s -t 10")
+    print("You guessed wrong! Luckily I will only shut you down this time...")
 else:
     print("You win!")
     exit()
